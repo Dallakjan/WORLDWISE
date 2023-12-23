c1893e4a1849224103db6cb437e453c5aeeda71c
Hajk Dallakjan <hajk.dallakjan@bold.com>
2023-12-23 19:41:46 +0100

    first commit

---
+++ src/components/Map.jsx
@@ -0,0 +1,26 @@
+import { useSearchParams } from 'react-router-dom';
+import styles from './Map.module.css';
+
+const Map = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const lat = searchParams.get('lat');
+  const lng = searchParams.get('lng');
+
+  return (
+    <div className={styles.mapContainer}>
+      <h1>Map</h1>
+      <h1>
+        Position: {lat}, {lng}
+      </h1>
+      <button
+        onClick={() => {
+          setSearchParams({ lat: 40.7128, lng: -74.006 });
+        }}
+      >
+        Change Position
+      </button>
+    </div>
+  );
+};
+
+export default Map;
